8d061a51420a890104b44687101dc774af8a7da9
RafsanzzZ69 <mahir.muntasir.rafsan@g.bracu.ac.bd>
2025-04-27 22:57:55 +0600

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,23 @@
+<?php
+// first of all, we need to connect to the database
+require_once('DConnect.php');
+
+// we need to check if the input in the form textfields are not empty
+if (isset($_POST['name']) && isset($_POST['pass'])) {
+	// write the query to check if this username and password exists in our database
+	$u = $_POST['name'];
+	$p = $_POST['pass'];
+	$sql = "SELECT * FROM user WHERE username = '$u' AND password = '$p'";
+
+	// Execute the query
+	$result = mysqli_query($conn, $sql);
+
+	// check if it returns an empty set
+	if (mysqli_num_rows($result) != 0) {
+		echo "LET HIM ENTER;";
+	} else {
+		echo "Username or Password is wrong";
+	}
+}
+
+?> 
