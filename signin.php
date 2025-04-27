4e978a648d4869c3bb57f27ef5ce400d06106ac8
RafsanzzZ69 <mahir.muntasir.rafsan@g.bracu.ac.bd>
2025-04-27 23:33:39 +0600

    Update signin.php

--- signin.php
+++ signin.php
@@ -7,17 +7,21 @@ if (isset($_POST['fname']) && isset($_POST['pass'])) {
 	// write the query to check if this username and password exists in our database
 	$u = $_POST['fname'];
 	$p = $_POST['pass'];
-	$sql = "SELECT * FROM user WHERE username = '$u' AND password = '$p'";
+	$sql = "SELECT * FROM users WHERE username = '$u' AND password = '$p'";
 
 	// Execute the query
 	$result = mysqli_query($conn, $sql);
 
 	// check if it returns an empty set
 	if (mysqli_num_rows($result) != 0) {
-		echo "LET HIM ENTER";
-	} else {
-		echo "Username or Password is wrong";
+		//echo "LET HIM ENTER";
+		header ("Location : home.php");
+	}
+    else {
+		//echo "Username or Password is wrong";
+		header("Location: index.php");
 	}
 }
 
 ?> 
+     
